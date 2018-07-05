1e5e24866a658780786934803a1f149790b1a1ea
Jeff Ashton <JeffAshton@users.noreply.github.com>
2018-07-04 23:02:42 -0400

    Simplifying instance catalog (#4)
    * Simplifying instance catalog

--- src/instanceCatalog/instanceCatalogParser.js
+++ src/instanceCatalog/instanceCatalogParser.js
@@ -4,17 +4,17 @@ const InstanceCatalog = require( './InstanceCatalog.js' );
 module.exports = function parseInstanceCatalog( instances ) {
 	
 	const instanceIdsByName = new Map();
-	const tenantIdsByDomainName = new Map();
+	const tenantIdsByDomain = new Map();
 
 	_.forEach( instances, instance => {
-		instanceIdsByName.set( instance.ClientName, instance.InstanceId );
+		instanceIdsByName.set( instance.name, instance.instanceId );
 
-		_.forIn( instance.Orgs, org => {
-			_.forIn( org.HostNames, ( domain, domainName ) => {
-				tenantIdsByDomainName.set( domainName, org.TenantId );
+		_.forEach( instance.tenants, tenant => {
+			_.forEach( tenant.domains, domain => {
+				tenantIdsByDomain.set( domain, tenant.tenantId );
 			} );
 		} );
 	} );
 
-	return new InstanceCatalog( instanceIdsByName, tenantIdsByDomainName );
+	return new InstanceCatalog( instanceIdsByName, tenantIdsByDomain );
 };

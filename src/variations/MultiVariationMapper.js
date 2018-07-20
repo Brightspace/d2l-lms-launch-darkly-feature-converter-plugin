410b1f44f5be155f3c15f893bc2cb6bf2d5d07d0
Jeff Ashton <JeffAshton@users.noreply.github.com>
2018-07-20 17:22:03 -0400

    Adding support for array multi variation definitions (#12)
    * Adding support for array multi variation definitions

--- src/variations/MultiVariationMapper.js
+++ src/variations/MultiVariationMapper.js
@@ -1,39 +1,110 @@
 const _ = require( 'lodash' );
+const duplicatesDeep = require( '../utils.js' ).duplicatesDeep;
 const VariationIndexMap = require( './VariationIndexMap.js' );
+
+function validateAllSameType( values ) {
+
+	let valueType;
+
+	_.forEach( values, value => {
+
+		if( _.isUndefined( valueType ) ) {
+			valueType = typeof ( value );
+
+		} else if( valueType !== typeof ( value ) ) {
+			throw new Error( 'Variation values must all be of the same type' );
+		}
+	} );
+}
+
+function mapVariation( variation ) {
+
+	const mappedVariation = _.pick( variation, [
+		'name',
+		'description',
+		'value'
+	] );
+
+	return mappedVariation;
+}
+
+function mapVariationsFromObject( definition ) {
+
+	const allValues = _.map( _.values( definition ), d => d.value );
+	validateAllSameType( allValues );
+
+	const duplicateValues = duplicatesDeep( allValues );
+	if( duplicateValues.length > 0 ) {
+
+		const msg = `Duplicate variation values: ${ _.join( duplicateValues, ', ' ) }`;
+		throw new Error( msg );
+	}
+
+	const indexes = {};
+	const variations = [];
+
+	let index = 0;
+
+	_.forIn( definition, ( variation, key ) => {
+		indexes[ key ] = index;
+
+		const mappedVariation = mapVariation( variation );
+		variations.push( mappedVariation );
+
+		index++;
+	} );
+
+	return {
+		indexes: new VariationIndexMap( indexes ),
+		variations
+	};
+}
+
+function mapVariationsFromArray( definition ) {
+
+	const duplicateKeys = duplicatesDeep(
+		_.map( definition, d => d.key )
+	);
+	if( duplicateKeys.length > 0 ) {
+
+		const msg = `Duplicate variation keys: ${ _.join( duplicateKeys, ', ' ) }`;
+		throw new Error( msg );
+	}
+
+	const allValues = _.map( definition, d => d.value );
+	validateAllSameType( allValues );
+
+	const duplicateValues = duplicatesDeep( allValues );
+	if( duplicateValues.length > 0 ) {
+
+		const msg = `Duplicate variation values: ${ _.join( duplicateValues, ', ' ) }`;
+		throw new Error( msg );
+	}
+
+	const indexes = {};
+	const variations = [];
+
+	definition.forEach( ( variation, index ) => {
+		indexes[ variation.key ] = index;
+
+		const mappedVariation = mapVariation( variation );
+		variations.push( mappedVariation );
+	} );
+
+	return {
+		indexes: new VariationIndexMap( indexes ),
+		variations
+	};
+}
 
 module.exports = class MultiVariationMapper {
 
 	mapVariations( definition ) {
 
-		let valueType;
-		const indexes = {};
-		const variations = [];
+		if( _.isArray( definition ) ) {
+			return mapVariationsFromArray( definition );
+		}
 
-		let index = 0;
-
-		_.forIn( definition, ( variation, key ) => {
-			indexes[key] = index;
-
-			if( _.isUndefined( valueType ) ) {
-				valueType = typeof( variation.value );
-
-			} else if( valueType !== typeof( variation.value ) ) {
-				throw new Error( 'Variation values must all be of the same type' );
-			}
-
-			const mappedVariation = _.pick( variation, [
-				'name',
-				'description',
-				'value'
-			] );
-
-			variations.push( mappedVariation );
-			index++;
-		} );
-
-		return {
-			indexes: new VariationIndexMap( indexes ),
-			variations
-		};
+		return mapVariationsFromObject( definition );
 	}
 };
--- test/MultiVariationMapperTests.js
+++ test/MultiVariationMapperTests.js
@@ -6,129 +6,326 @@ const mapper = new MultiVariationMapper();
 describe( 'MultiVariationMapper', function() {
 	describe( 'mapVariations', function() {
 
-		it( 'should map variations', function() {
+		describe( 'object definition', function() {
+			it( 'should map variations', function() {
 
-			const result = mapper.mapVariations( {
-				red: {
-					name: 'Red',
-					description: 'Things are on fire',
-					value: 0
-				},
-				green: {
-					name: 'Green',
-					description: 'Things are all ok',
-					value: 1
-				},
-				blue: {
-					name: 'Blue',
-					description: 'Things are unknown',
-					value: 2
-				}
+				const result = mapper.mapVariations( {
+					red: {
+						name: 'Red',
+						description: 'Things are on fire',
+						value: 0
+					},
+					green: {
+						name: 'Green',
+						description: 'Things are all ok',
+						value: 1
+					},
+					blue: {
+						name: 'Blue',
+						description: 'Things are unknown',
+						value: 2
+					}
+				} );
+
+				assert.equal(
+					result.indexes.getIndex( 'red' ),
+					0,
+					'red should map to index 0'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'green' ),
+					1,
+					'green should map to index 1'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'blue' ),
+					2,
+					'blue should map to index 2'
+				);
+
+				assert.deepEqual( result.variations, [
+					{
+						name: 'Red',
+						description: 'Things are on fire',
+						value: 0
+					},
+					{
+						name: 'Green',
+						description: 'Things are all ok',
+						value: 1
+					},
+					{
+						name: 'Blue',
+						description: 'Things are unknown',
+						value: 2
+					}
+				] );
 			} );
 
-			assert.equal(
-				result.indexes.getIndex( 'red' ),
-				0,
-				'red should map to index 0'
-			);
+			it( 'should throw if value types mismatch', function() {
 
-			assert.equal(
-				result.indexes.getIndex( 'green' ),
-				1,
-				'green should map to index 1'
-			);
+				assert.throws(
+					() => {
+						mapper.mapVariations( {
+							off: {
+								name: 'Off',
+								description: 'The switch is off',
+								value: false
+							},
+							on: {
+								name: 'On',
+								description: 'The switch is on',
+								value: 'on'
+							}
+						} );
+					},
+					/Variation values must all be of the same type/
+				);
 
-			assert.equal(
-				result.indexes.getIndex( 'blue' ),
-				2,
-				'blue should map to index 2'
-			);
-
-			assert.deepEqual( result.variations, [
-				{
-					name: 'Red',
-					description: 'Things are on fire',
-					value: 0
-				},
-				{
-					name: 'Green',
-					description: 'Things are all ok',
-					value: 1
-				},
-				{
-					name: 'Blue',
-					description: 'Things are unknown',
-					value: 2
-				}
-			] );
-		} );
-
-		it( 'should throw if value types mismatch', function() {
-
-			assert.throws(
-				() => {
-					mapper.mapVariations( {
-						off: {
-							name: 'Off',
-							description: 'The switch is off',
-							value: false
-						},
-						on: {
-							name: 'On',
-							description: 'The switch is on',
-							value: 'on'
-						}
-					} );
-				},
-				/Variation values must all be of the same type/
-			);
-
-		} );
-
-		it( 'should map just values', function() {
-
-			const result = mapper.mapVariations( {
-				red: {
-					value: 0
-				},
-				green: {
-					value: 1
-				},
-				blue: {
-					value: 2
-				}
 			} );
 
-			assert.equal(
-				result.indexes.getIndex( 'red' ),
-				0,
-				'red should map to index 0'
-			);
+			it( 'should map just values', function() {
 
-			assert.equal(
-				result.indexes.getIndex( 'green' ),
-				1,
-				'green should map to index 1'
-			);
+				const result = mapper.mapVariations( {
+					red: {
+						value: 0
+					},
+					green: {
+						value: 1
+					},
+					blue: {
+						value: 2
+					}
+				} );
 
-			assert.equal(
-				result.indexes.getIndex( 'blue' ),
-				2,
-				'blue should map to index 2'
-			);
+				assert.equal(
+					result.indexes.getIndex( 'red' ),
+					0,
+					'red should map to index 0'
+				);
 
-			assert.deepEqual( result.variations, [
-				{
-					value: 0
-				},
-				{
-					value: 1
-				},
-				{
-					value: 2
-				}
-			] );
+				assert.equal(
+					result.indexes.getIndex( 'green' ),
+					1,
+					'green should map to index 1'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'blue' ),
+					2,
+					'blue should map to index 2'
+				);
+
+				assert.deepEqual( result.variations, [
+					{
+						value: 0
+					},
+					{
+						value: 1
+					},
+					{
+						value: 2
+					}
+				] );
+			} );
+
+			it( 'should throw if values duplicated', function() {
+
+				assert.throws(
+					() => {
+						mapper.mapVariations( {
+							off: {
+								name: 'Off',
+								value: 1
+							},
+							on: {
+								name: 'On',
+								value: 1
+							}
+						} );
+					},
+					/^Duplicate variation values: 1$/
+				);
+
+			} );
 		} );
 
+		describe( 'array definition', function() {
+			it( 'should map variations', function() {
+
+				const result = mapper.mapVariations( [
+					{
+						key: 'red',
+						name: 'Red',
+						description: 'Things are on fire',
+						value: 0
+					},
+					{
+						key: 'green',
+						name: 'Green',
+						description: 'Things are all ok',
+						value: 1
+					},
+					{
+						key: 'blue',
+						name: 'Blue',
+						description: 'Things are unknown',
+						value: 2
+					}
+				] );
+
+				assert.equal(
+					result.indexes.getIndex( 'red' ),
+					0,
+					'red should map to index 0'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'green' ),
+					1,
+					'green should map to index 1'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'blue' ),
+					2,
+					'blue should map to index 2'
+				);
+
+				assert.deepEqual( result.variations, [
+					{
+						name: 'Red',
+						description: 'Things are on fire',
+						value: 0
+					},
+					{
+						name: 'Green',
+						description: 'Things are all ok',
+						value: 1
+					},
+					{
+						name: 'Blue',
+						description: 'Things are unknown',
+						value: 2
+					}
+				] );
+			} );
+
+			it( 'should throw if value types mismatch', function() {
+
+				assert.throws(
+					() => {
+						mapper.mapVariations( [
+							{
+								key: 'off',
+								name: 'Off',
+								description: 'The switch is off',
+								value: false
+							},
+							{
+								key: 'on',
+								name: 'On',
+								description: 'The switch is on',
+								value: 'on'
+							}
+						] );
+					},
+					/Variation values must all be of the same type/
+				);
+
+			} );
+
+			it( 'should map just values', function() {
+
+				const result = mapper.mapVariations( [
+					{
+						key: 'red',
+						value: 0
+					},
+					{
+						key: 'green',
+						value: 1
+					},
+					{
+						key: 'blue',
+						value: 2
+					}
+				] );
+
+				assert.equal(
+					result.indexes.getIndex( 'red' ),
+					0,
+					'red should map to index 0'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'green' ),
+					1,
+					'green should map to index 1'
+				);
+
+				assert.equal(
+					result.indexes.getIndex( 'blue' ),
+					2,
+					'blue should map to index 2'
+				);
+
+				assert.deepEqual( result.variations, [
+					{
+						value: 0
+					},
+					{
+						value: 1
+					},
+					{
+						value: 2
+					}
+				] );
+			} );
+
+			it( 'should throw if keys duplicated', function() {
+
+				assert.throws(
+					() => {
+						mapper.mapVariations( [
+							{
+								key: 'off',
+								name: 'Off',
+								value: 0
+							},
+							{
+								key: 'off',
+								name: 'On',
+								value: 1
+							}
+						] );
+					},
+					/^Duplicate variation keys: off$/
+				);
+			} );
+
+			it( 'should throw if values duplicated', function() {
+
+				assert.throws(
+					() => {
+						mapper.mapVariations( [
+							{
+								key: 'off',
+								name: 'Off',
+								value: 1
+							},
+							{
+								key: 'on',
+								name: 'On',
+								value: 1
+							}
+						] );
+					},
+					/^Duplicate variation values: 1$/
+				);
+			} );
+		} );
 	} );
 } );
